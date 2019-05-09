f506dd4bb2d6a288cc9af8e25b1ae847132841ba
Kate Kim <katekim5809@gmail.com>
2019-05-09 12:52:25 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,28 @@
 export function managePresents(state, action){
+
+  switch(action.type){
+    case "INCREASE":
+    return {
+      ...state,
+      numberOfPresents: state.numberOfPresents+1
+    }
+    default:
+      return state
+  }
 }
+
+
+// export function managePresents(state = {
+// numberOfPresents: 0,
+// }, action) {
+//   switch(action.type) {
+//
+//      case "INCREASE":
+//       return Object.assign({}, state, {
+//         numberOfPresents: state.numberOfPresents + 1
+//       });
+//
+//      default:
+//       return state;
+//   }
+// };
