1d0e546b286974ed2dd5aa805f0e0a098ab6835c
rrosztoczy <rrosztoczy@gmail.com>
2019-05-09 12:55:44 -0400

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
 export function manageFriends(state, action){
+  // { friends: [ { name: 'Avi', hometown: 'NYC', id: 100 }, { name: 'Joe', hometown: 'Boston', id: 101 } ]
+
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {...state, friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+      let newFriends = state.friends.filter(function( obj ) {
+      return obj.id !== action.id;
+      })
+      return {...state, friends: [...newFriends]}
+    default:
+      return state;
+  }
 }
+
+// {
+//   type: "ADD_FRIEND",
+//   friend: {
+//     name: 'Joe',
+//     hometown: 'Boston',
+//     id: 101
+//   }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
+
+
 export function managePresents(state, action){
+  
+  // let state = {count: 0}
+
+  switch (action.type) {
+    case 'INCREASE':
+      return {numberOfPresents: state.numberOfPresents + 1}
+    default:
+      return state;
+  }
 }
